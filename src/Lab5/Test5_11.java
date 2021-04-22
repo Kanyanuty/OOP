bb6b3cea91765eb5928616fb58e6f67ad86dc4c3
Kanyanuty <6206021612148@fitm.kmutnb.ac.th>
2021-04-22 17:36:20 +0700

    lab12

--- src/Lab5/Test5_11.java
+++ src/Lab5/Test5_11.java
@@ -3,17 +3,20 @@ import javax.swing.*;
 
 public class Test5_11 {
     public static void main(String[] args) {
+        //           0  1  2  3  4  5  6  7 8  9
         int[] array={99,45,36,21,87,65,34,1,93,77};
         String str;
         boolean done = true;
         //array = new int[20];
         //setData(array);
-        do {
+        do {//                                             30 
             str = JOptionPane.showInputDialog("Enter value to search :");
             int key = Integer.parseInt(str);
+                                       // 9     30
             int result = linearSearch(array, key);
             if (key != -1) {
                 if (result != -1)
+                                                                                 //   3
                     JOptionPane.showMessageDialog(null, "Value found in element " + result, "Linear Search",
                             JOptionPane.INFORMATION_MESSAGE);
                 else
@@ -28,16 +31,19 @@ public class Test5_11 {
         for (int c = 0; c < a.length; c++)
             a[c] = rnd(1, 100);
     }*/
-
+//                                      9       30
     public static int linearSearch(int a[], int key) {
+//                10      10        10
         for (int c = 0; c < a.length; c++)
+    //            9      30
             if (a[c] == key)
+            //          3
                 return (c);
         return (-1);
     }
 
-    public static int rnd(int st, int ed) {
+    /*public static int rnd(int st, int ed) {
         int offset = ed - st + 1;
         return (st + (int) (Math.random() * offset));
-    }
+    }*/
 }
